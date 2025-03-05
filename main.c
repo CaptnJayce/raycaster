eb287725387a0c1c835052eba1a27bcba9064b0b
CaptnJayce <captn.dev@protonmail.com>
2025-03-05 22:18:30 +0000

    horizontal line check

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
-#include <raylib.h>
 #include <math.h>
+#include <raylib.h>
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 800
@@ -9,7 +9,6 @@ struct Player {
   Color colour;
   int w;
   int h;
-  float speed;
 
   // for rotation
   float pdx;
@@ -20,30 +19,66 @@ struct Player {
 int mapX = 10;
 int mapY = 10;
 int mapS = 80;
-int map[] =
-{
-  1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-  1, 0, 1, 0, 0, 0, 0, 0, 0, 1,
-  1, 0, 1, 0, 0, 0, 1, 1, 0, 1,
-  1, 0, 1, 0, 0, 0, 0, 1, 0, 1,
-  1, 0, 1, 0, 0, 0, 0, 0, 0, 1,
-  1, 0, 0, 0, 0, 0, 1, 1, 1, 1,
-  1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-  1, 0, 1, 0, 1, 0, 0, 0, 0, 1,
-  1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-  1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-};
+int map[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+             1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1,
+             1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1,
+             1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1,
+             1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 
 void drawMap() {
-    for (int y = 0; y < mapY; y++) {
-        for (int x = 0; x < mapX; x++) {
-            if (map[y * mapX + x] == 1) {
-                DrawRectangle(x * mapS + 1, y * mapS + 1, mapS - 2, mapS - 2, WHITE);
-            } else {
-                DrawRectangle(x * mapS + 1, y * mapS + 1, mapS - 2, mapS - 2, BLACK);
-            }
-        }
+  for (int y = 0; y < mapY; y++) {
+    for (int x = 0; x < mapX; x++) {
+      if (map[y * mapX + x] == 1) {
+        DrawRectangle(x * mapS + 1, y * mapS + 1, mapS - 2, mapS - 2, WHITE);
+      } else {
+        DrawRectangle(x * mapS + 1, y * mapS + 1, mapS - 2, mapS - 2, BLACK);
+      }
     }
+  }
+}
+
+void drawRays(struct Player p) {
+  int r, mx, my, mp, dof;
+  float rx, ry, ra, xo, yo;
+
+  ra = p.pa;
+
+  for (r = 0; r < 1; r++) {
+    dof = 0;
+    float aTan = -1 / tan(ra);
+
+    if (ra > PI) {
+      ry = (((int)p.pos.y / mapS) * mapS) - 0.0001;
+      rx = (p.pos.y - ry) * aTan + p.pos.x;
+      yo = -mapS;
+      xo = -yo * aTan;
+    } else if (ra < PI) {
+      ry = (((int)p.pos.y / mapS) * mapS) + mapS;
+      rx = (p.pos.y - ry) * aTan + p.pos.x;
+      yo = mapS;
+      xo = -yo * aTan;
+    } else {
+      rx = p.pos.x;
+      ry = p.pos.y;
+      dof = 8;
+    }
+
+    while (dof < 8) {
+      mx = (int)(rx) / mapS;
+      my = (int)(ry) / mapS;
+      mp = my * mapX + mx;
+
+      if (mp > 0 && mp < mapX * mapY && map[mp] == 1) {
+        dof = 8;
+      } else {
+        rx += xo;
+        ry += yo;
+        dof += 1;
+      }
+    }
+
+    DrawLine(p.pos.x + 10, p.pos.y + 10, rx, ry, RED);
+  }
 }
 
 int main() {
@@ -55,21 +90,22 @@ int main() {
   p.colour = GREEN;
   p.w = 20;
   p.h = 20;
-  p.speed = 300.0;
+  p.pdx = cos(p.pa) * 5;
+  p.pdy = sin(p.pa) * 5;
 
   while (!WindowShouldClose()) {
     if (IsKeyDown(KEY_A)) {
       p.pa -= 0.1;
       if (p.pa < 0) {
-        p.pa += 2*PI;
+        p.pa += 2 * PI;
       }
       p.pdx = cos(p.pa) * 5;
       p.pdy = sin(p.pa) * 5;
     }
     if (IsKeyDown(KEY_D)) {
       p.pa += 0.1;
-      if (p.pa > 2*PI) {
-        p.pa -= 2*PI;
+      if (p.pa > 2 * PI) {
+        p.pa -= 2 * PI;
       }
       p.pdx = cos(p.pa) * 5;
       p.pdy = sin(p.pa) * 5;
@@ -89,7 +125,7 @@ int main() {
 
     drawMap();
     DrawRectangle(p.pos.x, p.pos.y, p.w, p.h, p.colour);
-    DrawLine(p.pos.x, p.pos.y, p.pos.x+p.pdx*5, p.pos.y+p.pdy*5, RED);
+    drawRays(p);
     EndDrawing();
   }
 
